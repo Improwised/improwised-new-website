26ba0d4b558b4c238371974666de09bde70c3397
ridham-parmar <ridham.parmar@improwised.com>
2025-12-18 18:43:16 +0530

    fix: navigate over hover on blog and case study

--- newapp/src/components/common/BlogsCarousel.svelte
+++ newapp/src/components/common/BlogsCarousel.svelte
@@ -4,6 +4,7 @@
   export let blogs: any[] = [];
 </script>
 
+
 <Carousel.Root
 opts={{
   align: "start",
@@ -18,33 +19,31 @@ plugins={[
 >
   <Carousel.Content>
     {#each blogs as featuredBlog, i (i)}
-
       <Carousel.Item class="md:basis-1/2 lg:basis-1/3">
-        <div class="flex items-center gap-2">
-          <a href={"/blog/" + featuredBlog.data.slug + "/"} class="bg-white rounded-md self-stretch flex items-center min-w-[70px]"> 
-            <img
-              alt="feature"
-              class="p-1 shadow-nav"
-              width={70}
-              height={70}
-              src={featuredBlog.data.image.src}
+        <a href={"/blog/" + featuredBlog.data.slug + "/"}>
+          <div class="flex items-center gap-2">
+            <div class="bg-white rounded-md self-stretch flex items-center min-w-[70px]"> 
+              <img
+                alt="feature"
+                class="p-1 shadow-nav"
+                width={70}
+                height={70}
+                src={featuredBlog.data.image.src}
+              >
+            </div>
+            <p
+              class="text-left text-fourth-color text-[12px] md:text-[14px] md:leading-[18px] tracking-tight"
             >
-          </a>
-          <p
-            class="text-left text-fourth-color text-[12px] md:text-[14px] md:leading-[18px] tracking-tight"
-          >
-            {featuredBlog.data.tags[0]}
-            <br />
-            <span
-              class="text-left mt-2 font-manrope text-[14px] text-fifth-color md:text-[16px] md:leading-[22px] tracking-tight"
-            >
-            <a href={"/blog/" + featuredBlog.data.slug + "/"}>
+              {featuredBlog.data.tags[0]}
+              <br />
+              <span
+                class="text-left mt-2 font-manrope text-[14px] text-fifth-color md:text-[16px] md:leading-[22px] tracking-tight"
+              >
               {featuredBlog.data.title}
-             </a>
-            </span>
-          </p>
-        </div>
-
+              </span>
+            </p>
+          </div>
+        </a>
       </Carousel.Item>
     {/each}
   </Carousel.Content>

014175c5b4e5b18a779bd19be20c2781ba33ef44
ridham-parmar <ridham.parmar@improwised.com>
2025-12-22 20:05:58 +0530

    feat: add full page search in mobile view

--- newapp/src/components/common/Dialog.svelte
+++ newapp/src/components/common/Dialog.svelte
@@ -1,22 +1,38 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { Button } from "$lib/components/ui/button";
   import * as Dialog from "$lib/components/ui/dialog/index.js";
   import { Search } from "lucide-svelte";
+
+  let open = false;
+
+  onMount(() => {
+    const handleKeydown = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === 'k') {
+        event.preventDefault();
+        open = true;
+      }
+    };
+
+    window.addEventListener('keydown', handleKeydown);
+
+    return () => window.removeEventListener('keydown', handleKeydown);
+  });
 </script>
 
-<Dialog.Root>
-  <Dialog.Trigger class="button-bg rounded-md outline-0">
+<Dialog.Root bind:open={open}>
+  <Dialog.Trigger class="rounded-md outline-0">
     <Button
-      class="button-bg flex gap-2 cursor-pointer"
+      class="hidden button-bg lg:flex gap-2 cursor-pointer"
       aria-label="Open full page search modal"
     >
      <Search class="h-4 w-4" />
-      Ctrl K
+      <span>Ctrl K</span>
     </Button>
-    <!-- <span class="hidden sm:inline">Ctrl K</span> -->
+    <Search class="h-5 w-5 lg:hidden cursor-pointer" />
   </Dialog.Trigger>
   <Dialog.Content
-    class="sm:max-w-3xl max-h-[600px] search-dialog-content flex flex-col"
+    class="max-w-3xl max-h-[600px] search-dialog-content flex flex-col"
   >
     <Dialog.Header class="border-b border-[var(--card-border)]">
       <Dialog.Title class="text-2xl font-bold text-[var(--text-first-color)]"

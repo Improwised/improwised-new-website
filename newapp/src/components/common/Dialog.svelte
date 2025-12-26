34b7a16c662dbd3c20cf2954b9e7217486f08e5c
ridham-parmar <ridham.parmar@improwised.com>
2025-12-26 11:57:25 +0530

    fix: modified UI for search dialog

--- newapp/src/components/common/Dialog.svelte
+++ newapp/src/components/common/Dialog.svelte
@@ -1,41 +1,26 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import { Button } from "$lib/components/ui/button";
   import * as Dialog from "$lib/components/ui/dialog/index.js";
   import { Search } from "lucide-svelte";
 
-  let open = false;
-
-  onMount(() => {
-    const handleKeydown = (event: KeyboardEvent) => {
-      if (event.ctrlKey && event.key === 'k') {
-        event.preventDefault();
-        open = true;
-      }
-    };
-
-    window.addEventListener('keydown', handleKeydown);
-
-    return () => window.removeEventListener('keydown', handleKeydown);
-  });
 </script>
 
-<Dialog.Root bind:open={open}>
+<Dialog.Root>
   <Dialog.Trigger class="rounded-md outline-0">
     <Button
       class="hidden button-bg lg:flex gap-2 cursor-pointer"
       aria-label="Open full page search modal"
     >
      <Search class="h-4 w-4" />
-      <span>Ctrl K</span>
+      <span>Search</span>
     </Button>
     <Search class="h-5 w-5 lg:hidden cursor-pointer" />
   </Dialog.Trigger>
   <Dialog.Content
-    class="max-w-3xl max-h-[600px] search-dialog-content flex flex-col"
+    class="max-w-[95vw] max-h-[80vh] sm:max-w-2xl lg:max-w-3xl sm:max-h-[600px] search-dialog-content flex flex-col"
   >
     <Dialog.Header class="border-b border-[var(--card-border)]">
-      <Dialog.Title class="text-2xl font-bold text-[var(--text-first-color)]"
+      <Dialog.Title class="text-lg sm:text-xl lg:text-2xl font-bold text-[var(--text-first-color)]"
         >Search</Dialog.Title
       >
     </Dialog.Header>
